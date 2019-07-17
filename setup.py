b918325f37968c6ec97f52577d4e5b3fd9143852
idl3r <idler1984@gmail.com>
2019-07-17 13:45:50 +0800

    replace interpreter

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 
 from __future__ import print_function
 from distutils.core import setup, Extension
--- test/arch/x86/sem.py
+++ test/arch/x86/sem.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 #-*- coding:utf-8 -*-
 
 # Loosely based on ARM's sem.py
